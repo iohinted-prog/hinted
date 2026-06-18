399bb5d3e4ed4c29262b0ca562a989064e3f8976
iohinted-prog <iohinted@gmail.com>
2026-06-18 12:05:45 +0100

    Replace useMemo with useEffect in FeedClient.js

--- app/feed/FeedClient.js
+++ app/feed/FeedClient.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { createClient } from "../../lib/supabase/client";
 
 const demoMode = true;
