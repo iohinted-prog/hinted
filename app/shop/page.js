4142efaf880001e6fb5f057e4707e8ad3d254ca8
iohinted-prog <iohinted@gmail.com>
2026-06-24 00:43:01 +0100

    Remove unused SiteHeader and SiteFooter imports
    Removed imports for SiteHeader and SiteFooter components.

--- app/shop/page.js
+++ app/shop/page.js
@@ -4,8 +4,6 @@ import { useEffect, useMemo, useState } from "react";
 import Script from "next/script";
 import { createClient } from "../../lib/supabase/client";
 import { useCurrencyFormatter } from "../../lib/useCurrencyFormatter";
-import AppHeader from "../components/SiteHeader";
-import AppFooter from "../components/SiteFooter";
 
 const INTEREST_OPTIONS = [
   "Home",
