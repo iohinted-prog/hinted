fd1bff21fffd12d66bdc0fbe0162589eb0e53153
iohinted-prog <iohinted@gmail.com>
2026-06-24 19:45:11 +0100

    Add metadata for page details and verification
    Added metadata for page title, description, and site verification.

--- app/page.js
+++ app/page.js
@@ -3,6 +3,15 @@ import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 import HomePageClient from "./components/HomePageClient";
 
+export const metadata = {
+  title: "Hinted.io | Never forget. Always thoughtful.",
+  description:
+    "Hinted helps you remember important moments, save better gift ideas, build circles, and explore curated shopping with help from your friends.",
+  other: {
+    "impact-site-verification": "e9b128fe-f48f-4547-98f7-037ee4183d82",
+  },
+};
+
 export default async function Page() {
   const cookieStore = await cookies();
 
