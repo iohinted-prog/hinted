f5dd202aa5a1170d43c180ab77cf8e63c53bef52
iohinted-prog <iohinted@gmail.com>
2026-07-23 17:34:28 +0000

    fix: add deploymentId to next.config to fix chunk loading on new deploys

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  deploymentId: process.env.VERCEL_DEPLOYMENT_ID || process.env.GIT_SHA || undefined,
 };
 
 export default nextConfig;
